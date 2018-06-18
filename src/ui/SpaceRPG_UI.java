b6c97bfbdb447ba056c9513127bcb77d6d4d556e
Karel De Smet <karel@Karel.be>
2018-06-18 23:21:14 +0200

    worked on equipment for weapons

--- src/ui/SpaceRPG_UI.java
+++ src/ui/SpaceRPG_UI.java
@@ -4,6 +4,10 @@ public class SpaceRPG_UI {
 	public static void main(String[] args) {
 		Player player = new Player("player");
 		Weapon backupWeapon = new Weapon("Backup weapon");
-		Visor helmet = new Visor("Backup helmet");
+		System.out.println("player damage:"+player.getDamage());
+		System.out.println("weapon damage:" +backupWeapon.getDamage());
+		player.equipWeapon(backupWeapon);
+		System.out.println("equipping weapon "+backupWeapon.getName());
+		System.out.println("player damage:"+player.getDamage());
 	}
 }
